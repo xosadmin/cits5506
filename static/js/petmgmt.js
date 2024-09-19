9880d138e056c0ba44c58e9c35a67c90ae75606c
xosadmin <9164356+xosadmin@users.noreply.github.com>
2024-09-19 15:28:20 +0800

    Fix cannot find element error

--- static/js/petmgmt.js
+++ static/js/petmgmt.js
@@ -1,13 +1,18 @@
-document.getElementById("regNewPet").addEventListener("click", function() {
-    window.location.href = "/addpet";
-});
+document.addEventListener("DOMContentLoaded", function() {
+    var regNewPetButton = document.getElementById("regNewPet");
+    if (regNewPetButton) {
+        regNewPetButton.addEventListener("click", function() {
+            window.location.href = "/addpet";
+        });
+    }
 
-var deleteLinks = document.querySelectorAll(".delete-link");
-deleteLinks.forEach(function(link) {
-    link.addEventListener("click", function(event) {
-        var confirmation = confirm("Are you sure to de-register this pet? All info related to this pet will be deleted. This is not undoable. Press OK to continue.");
-        if (!confirmation) {
-            event.preventDefault();
-        }
+    var deleteLinks = document.querySelectorAll(".delete-link");
+    deleteLinks.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            var confirmation = confirm("Are you sure to de-register this pet? All info related to this pet will be deleted. This is not undoable. Press OK to continue.");
+            if (!confirmation) {
+                event.preventDefault();
+            }
+        });
     });
 });
